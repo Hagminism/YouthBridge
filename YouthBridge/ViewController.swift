624ab844dd39bff6441122c2caa461dac547c1cb
Hagminism <iilhm110629@gmail.com>
2026-06-02 14:06:57 +0900

    Initial Commit

---
+++ YouthBridge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouthBridge
+//
+//  Created by 이학민 on 6/2/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
